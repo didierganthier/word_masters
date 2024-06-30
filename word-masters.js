ed0f7fc864eaf7d2dde92d3b3235f90dee918b7d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-06-30 16:08:10 -0400

    Log word to console

--- word-masters.js
+++ word-masters.js
@@ -7,6 +7,13 @@ async function init() {
     let currentRow = 0;
 
 
+    const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+    const resObj = await res.json();
+    const word = resObj.word.toUpperCase();
+
+    console.log(word);
+
+
     function addLetter(letter) {
         if(currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
